88545946137acc59c2591f34d9d5ee9c132fe046
zfishere <zfishere@gmail.com>
2017-04-02 11:14:31 +0800

    add comments

--- startUp.py
+++ startUp.py
@@ -15,12 +15,14 @@ userInfoCol = ['userId','gender','job','education','marriage','residentialType']
 userInfo = pd.read_csv('user_info_train.csv',index_col = False, header  = None, names = userInfoCol)
 # 银行流水记录：用户id,时间戳,交易类型,交易金额,工资收入标记
 bankDetailCol = ['userId','timeStamp','transType','transAmount','salaryIncome']
-bankDetail = pd.read_csv('bank_detail_train.csv',index_col = False, header  = None, names = bandDetailCol)
+bankDetail = pd.read_csv('bank_detail_train.csv',index_col = False, header  = None, names = bankDetailCol)
 # 信用卡账单记录：用户id，账单时间戳，银行id，上期账单金额， 上期还款金额，信用卡额度，本期账单余额，
 # 本期账单最低还款，消费笔数,本期账单金额，调整金额，循环利息，可用余额，预借现金额度， 还款状态
+# possible noise : bankId
+# 加入 用户使用年数； grouping by time range rows of bankDetail, billDetail of same user ; 
 billDetailCol = \
     ['userId','billTimeStmp','bankId','lastBillAmt','lastPaidAmt','creditAmount','remainedBalThisMon',\
-    'minPayThisMon','#ofTrans','balThisMon','ajtedAmt','evolInst','remainBal','cashedAmt','payStus']
+    'minPayThisMon','#ofTrans','balThisMon','ajtedAmt','evolInst','remainBal','cashCredictLimit','payStus']
 billDetail = pd.read_csv('bill_detail_train.csv',index_col = False, header  = None, names = billDetailCol)
  # 用户浏览行为: 用户id,时间戳,浏览行为数据,浏览子行为编号
 browseHistCol = ['userId','timeStmp','browsAct','browsId']
