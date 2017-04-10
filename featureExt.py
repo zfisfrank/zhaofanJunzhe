4d66cc5c67d0ac2348181759a31896ffb18e862a
zfishere <zfishere@gmail.com>
2017-04-10 18:05:06 +0800

    added project of pycharm

---
+++ featureExt.py
@@ -0,0 +1,15 @@
+# python3
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# 用户id,时间戳,交易类型,交易金额,工资收入标记
+bankDetailCol = ['userId','timeStamp','transType','transAmount','salaryIncome']
+bankDetail = pd.read_csv('../initData/bank_detail_train.txt',index_col =False,
+    header  = None, names = bankDetailCol).sort_values('userId')
+# bankDetail = bankDetail.reset_index()
+# bankDetail group
+bankDetailGp = bankDetail.groupby('userId')
+
+bankDetailTotalIncome = bankDetailGp.agg
