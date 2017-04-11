01be51f42c9090f49bfc36b6af05cbb61fd58d27
zfishere <zfishere@gmail.com>
2017-04-11 22:13:48 +0800

    pearson correlation coefficient calcualtion finished

--- modifyFun.py
+++ modifyFun.py
@@ -3,9 +3,18 @@
 import os
 import numpy as np
 import pandas as pd
+from scipy.stats import pearsonr
 
 # normalize to 0~1
-def normalize(dF):
-    dF = (dF - dF.min())/(dF.max() - dF.min())
-    dF = dF.dropna(how ='all',axis = 1)
-    return dF
+def normalize(df):
+    df = (df - df.min())/(df.max() - df.min())
+    df = df.dropna(how ='all',axis = 1)
+    return df
+
+# calculate all pearson correlation coefficient of columns in dataFrame with y(target)
+def pearCorr(df,y):
+    result =  df.apply(lambda x: pearsonr(x,y)).apply(pd.Series)
+    result.set_axis(1,['Pearson’s correlation coefficient','2-tailed p-value'])
+    return result
+
+
